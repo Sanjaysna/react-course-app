a007fbbee5f376a3e4f0f268d73db24a739918c3
Sanjaysna <sanjaysanthosh411@gmail.com>
2026-07-11 14:45:37 +0530

    Feature-add-navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+
+                    <a className="navbar-brand" href="/">
+                        Course App
+                    </a>
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+
+                            <a className="nav-link active" aria-current="page" href="/">
+                                Home
+                            </a>
+
+                            <a className="nav-link" href="/">
+                                Add Course
+                            </a>
+
+                            <a className="nav-link" href="/view">
+                                View Course
+                            </a>
+
+                        </div>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
